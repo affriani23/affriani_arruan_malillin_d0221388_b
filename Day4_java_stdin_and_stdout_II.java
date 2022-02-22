a1368e19c5e7d3c8d5731b85895be48b92184454
affriani23 <99956844+affriani23@users.noreply.github.com>
2022-02-22 19:04:38 +0700

    Day4_java_stdin_and_stdout_II.java

---
+++ Day4_java_stdin_and_stdout_II.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        double n = sc.nextDouble();
+        sc.nextLine();
+        String i = sc.nextLine();
+
+
+        System.out.println("String: " + i);
+        System.out.println("Double: " + n);
+        System.out.println("Int: " + a);
+    }
+}
